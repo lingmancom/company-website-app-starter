c97ff88f5d3b4bdf9d69e2d36984acaef3b5e6ac
阿杰 <53169820+laihaojie@users.noreply.github.com>
2024-02-27 18:03:03 +0800

    chore: update

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,6 @@ export const Api = {
   testGet: () => Get('/api/account/get'),
 
   // 测试接口Post
-
   tesPost: data => Post('/api/account/post', data),
 
   // 登录接口
@@ -11,16 +10,7 @@ export const Api = {
   // login: (data) => Post('/api/account/login', data),
 
   // 获取登录用户信息
-  getUserInfo: () => Get<UserInfo>('/api/account/info'),
-
-  // 获取项目列表
-  GetProjects: () => Get<Project[]>('/api/app/apilog/projects'),
-
-  // 改变APIroot
-  ChangeApiUrl: data => Post('/api/app/apilog/ChangeUrl1', data),
-
-  // 获取接口日志
-  GetApiLoginfo: data => Post<ApiLog[]>('/api/app/apilog/nextpage', data),
+  getUserInfo: () => Get('/api/account/info'),
 
   // 获取用户列表
   getUserList: () => Get('/api/account/get_all_info'),
@@ -46,60 +36,15 @@ export const Api = {
   // /api/project/user/projects
   getUserProjectList: data => Get('/api/project/user/projects', data),
 
-  // /api/proc/list
-  getProcList: data => Get('/api/proc/list', data),
+  // /api/project/list
+  getProjectList: data => Get('/api/project/list', data),
 
-  // /api/proc/save
-  saveProc: data => Post('/api/proc/save', data),
+  // /api/project/save
+  saveProject: data => Post('/api/project/save', data),
 
-  // /api/proc/delete
-  deleteProc: data => Post('/api/proc/delete', data),
+  // /api/project/delete
+  deleteProject: data => Post('/api/project/delete', data),
 
-  // /api/proc/role/save
-  saveProcRole: data => Post('/api/proc/role/save', data),
-
-  // /api/proc/role/list
-  getProcRoleList: data => Get('/api/proc/role/list', data),
-
-  // /api/stage/list
-  getStageList: data => Get('/api/stage/list', data),
-
-  // /api/stage/save
-  saveStage: data => Post('/api/stage/save', data),
-
-  // /api/stage/delete
-  deleteStage: data => Post('/api/stage/delete', data),
-
-  // /api/stage/sort
-  sortStage: data => Post('/api/stage/sort', data),
-
-  // /api/procins/list
-  getProcInsList: data => Get('/api/procins/list', data),
-
-  // /api/procins/save
-  saveProcIns: data => Post('/api/procins/save', data),
-
-  // /api/procins/delete
-  deleteProcIns: data => Post('/api/procins/delete', data),
-
-  // /api/procins/detail
-  getProcInsDetail: data => Get('/api/procins/detail', data),
-
-  // /api/contents/list
-  getContentsList: data => Get('/api/contents/list', data),
-
-  // /api/contents/save
-  saveContents: data => Post('/api/contents/save', data),
-
-  // /api/contents/delete
-  deleteContents: data => Post('/api/contents/delete', data),
-
-  // /api/category/list
-  getCategoryList: data => Get('/api/category/list', data),
-
-  // /api/category/save
-  saveCategory: data => Post('/api/category/save', data),
-
-  // /api/category/delete
-  deleteCategory: data => Post('/api/category/delete', data),
+  // /api/project/project/users
+  getProjectUserList: data => Get('/api/project/project/users', data),
 }
--- src/initialize.ts
+++ src/initialize.ts
@@ -1,21 +1,18 @@
 import { toLogin } from './utils'
 
-cfg.getApiRoot = () => {
-  return localStorage.getItem('port') || window.__BASE_API
-}
-cfg.getHttpHeader = () => ({
-  token: localGet('token') || '',
-})
-cfg.makeLogout = () => {
-  // 清除token
-  const accountStore = useAccountStore()
-  accountStore.clearToken()
-  toLogin()
-}
-cfg.showErrorToast = (msg: string) => ElMessage.error(msg)
-cfg.showInfoToast = (msg: string) => ElMessage.info(msg)
-
-service.interceptors.request.use((config) => {
-  config.timeout = 30 * 1000
-  return config
+Object.assign(cfg.request, {
+  getApiRoot: () => {
+    return window.__BASE_API
+  },
+  getHttpHeader: () => ({
+    token: localGet('token') || '',
+  }),
+  makeLogout: () => {
+    // 清除token
+    const accountStore = useAccountStore()
+    accountStore.clearToken()
+    toLogin()
+  },
+  showErrorToast: (msg: string) => ElMessage.error(msg),
+  showInfoToast: (msg: string) => ElMessage.info(msg),
 })
